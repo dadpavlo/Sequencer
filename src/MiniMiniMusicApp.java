ca3a38206ba7fa9be43eb4302691538743f51773
dadpavlo <dad.pavlo@gmail.com>
2022-01-24 03:24:02 +0300

    add BeatBox

--- src/MiniMiniMusicApp.java
+++ src/MiniMiniMusicApp.java
@@ -5,59 +5,74 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class MiniMiniMusicApp extends JPanel implements ActionListener{
-
-    JButton button;
+    int p = 1;
+    int pp = 4;
+    int note = 80;
+    GridBagConstraints grid;
+    Box f1;
+    JFrame frame1;
+    JButton button, b1;
     MiniMiniMusicApp mini;
     MyDrawPanel gg;
     JFrame frame;
 
     public static void main(String[] args){
         MiniMiniMusicApp gui = new MiniMiniMusicApp();
-        gui.go();
+
 
     }
     public void go(){
+        grid = new GridBagConstraints();
         gg = new MyDrawPanel();
         mini = new MiniMiniMusicApp();
         frame = new JFrame();
+        f1 = new Box(BoxLayout.X_AXIS);
         button = new JButton("click me");
+        b1 = new JButton("POP");
         button.addActionListener(this);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.getContentPane().add(BorderLayout.SOUTH, button);
-        frame.getContentPane().add(BorderLayout.CENTER,gg);
-        frame.setSize(100,100);
+//
+//        frame.getContentPane().add(BorderLayout.CENTER,gg);
+//        frame.add(f1);
+        frame.setSize(700,700);
         frame.setVisible(true);
-        mini.play();
 
     }
 
     public void play() {
         try {
-            Sequencer player = MidiSystem.getSequencer();
-            player.open();
-            Sequence seq = new Sequence(Sequence.PPQ, 4);
+            for (int i = 0; i < 20; i++) {
+                Sequencer player = MidiSystem.getSequencer();
+                player.open();
+                Sequence seq = new Sequence(Sequence.PPQ, 4);
 
                 Track track = seq.createTrack();
-                int note = (int) (Math.random() * (80-40)+30);
-                int t = (int) (Math.random() * (100-50)+50);
+                for (int j = 52; j < 70; j++) {
+                    int t = (int) (Math.random() * (100-50)+50);
 
-                int o = (int) (Math.random() * (32-16)+16);
+                    int o = (int) (Math.random() * (32-16)+16);
 
-                ShortMessage a = new ShortMessage();
-                a.setMessage(144,1,note,t);
-                MidiEvent noteOn = new MidiEvent(a,1);
-                track.add(noteOn);
+                    ShortMessage a = new ShortMessage();
+                    a.setMessage(144,1,j,50);
+                    MidiEvent noteOn = new MidiEvent(a,p);
+                    track.add(noteOn);
 
-                ShortMessage b = new ShortMessage();
-                b.setMessage(128,1,44,100);
-                MidiEvent noteOff = new MidiEvent(b, o);
-                track.add(noteOff);
+                    ShortMessage b = new ShortMessage();
+                    b.setMessage(128,1,44,50);
+                    MidiEvent noteOff = new MidiEvent(b, pp);
+                    track.add(noteOff);
+                    p= pp +1;
+                    pp = p+6;
+                }
+//                int note = (int) (Math.random() * (80-40)+30);
 
 
 
-            player.setSequence(seq);
-            player.start();
-            player.start();
+                player.setSequence(seq);
+                player.start();
+
+            }
 
 
         } catch (Exception ex) {
@@ -68,6 +83,7 @@ public class MiniMiniMusicApp extends JPanel implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         button.setText("playing note");
         mini.play();
+
 //        frame.repaint();
     }
 
